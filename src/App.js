d6f87f8cff5845208553e58009f867fc87e84191
sudexp <sudexp@icloud.com>
2018-11-15 11:57:51 +0200

    start page created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Navigation from './components/Navigation';
-import RegistrationForm from './components/RegistrationForm';
+// import Navigation from './components/Navigation';
+// import RegistrationForm from './components/RegistrationForm';
+import StartPage from './components/StartPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <RegistrationForm />
+        <StartPage />
       </div>
     );
   }
