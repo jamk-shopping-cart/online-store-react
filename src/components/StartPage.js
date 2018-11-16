d5a1b13820e52c9eadf92221c73e6fd58b5347d2
sudexp <sudexp@icloud.com>
2018-11-16 21:52:36 +0200

    components styles modified

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -5,14 +5,12 @@ import './StartPage.css';
 class StartPage extends Component {
   render() {
     return (
-      <div>
-        <div id="wrap">
-          <div id="center">ShoeFlex</div>
-          <div id="bottom">View Our Collection 2018</div>
-          <Link to="reg" id="link">
-            <i className="fas fa-2x fa-chevron-down" />
-          </Link>
-        </div>
+      <div id="wrap">
+        <div id="center">ShoeFlex</div>
+        <div id="bottom">View Our Collection 2018</div>
+        <Link to="reg" id="link">
+          <i className="fas fa-2x fa-chevron-down" />
+        </Link>
       </div>
     );
   }
