81332031c8a0eb86d1582b7d1da5e8cd1e73c07c
sudexp <sudexp@icloud.com>
2018-11-16 15:09:33 +0200

    routing modified, App, StartPage, Navigation changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Link } from './components/Router';
 import StartPage from './components/StartPage';
+import Navigation from './components/Navigation';
+import RegistrationForm from './components/RegistrationForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <StartPage />
+      <div>
+        <Route exact path="/" component={StartPage} />
+        <Route exact path="/reg" component={RegistrationForm} />
+        <Route exact path="/nav" component={Navigation} />
       </div>
     );
   }
