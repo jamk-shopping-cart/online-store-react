299c2fb6b13b3c7e83335baa0d491c83336094fa
sudexp <sudexp@icloud.com>
2018-11-16 23:29:30 +0200

    registration form modified

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
-import { Link } from './Router';
 import './RegistrationForm.css';
 
 class RegistrationForm extends Component {
@@ -9,15 +8,17 @@ class RegistrationForm extends Component {
       <div>
         <Navigation />
         <form>
-          <input id="email" type="email" placeholder="Email" autofocus required />
+          <input id="email" type="email" placeholder="Email" autoFocus required />
           <input id="password" type="password" placeholder="Password" required />
-          <input id="password" type="password" placeholder="Confirm password" required />
+          <input id="confirm_password" type="password" placeholder="Confirm password" required />
           <input id="name" type="text" placeholder="First Name" required />
           <input id="surname" type="text" placeholder="Last Name" required />
           <input id="phone" type="tel" placeholder="Phone" required />
           <input id="address" type="text" placeholder="Address" required />
           <input id="post" type="text" placeholder="Post" required />
-          <br />
+          <label>
+            <input id="confirm" type="checkbox" required /> I accept the privacy policy
+          </label>
           <input type="submit" value="Send" />
         </form>
       </div>
