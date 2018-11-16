a2b0724db8c41087aa4f0047845ea652fcfc258e
sudexp <sudexp@icloud.com>
2018-11-16 20:29:05 +0200

    added fontawesome as npm dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import React, { Component } from 'react';
 import { Route } from './components/Router';
 import StartPage from './components/StartPage';
