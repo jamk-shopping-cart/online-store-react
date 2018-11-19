cc493ac8e86faff794b0a07214d1f8a9fbdc6617
sudexp <sudexp@icloud.com>
2018-11-19 11:09:31 +0200

    startpage: fa-chevron-down modified

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -10,7 +10,7 @@ class StartPage extends Component {
         <div id="bottom">
           <p>View Our Collection 2018</p>
           <Link to="col">
-            <i className="fas fa-2x fa-chevron-down" />
+            <i className="fas fa fa-chevron-down" />
           </Link>
         </div>
       </div>
