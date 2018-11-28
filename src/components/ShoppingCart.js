6ac3ee7a6144dd6d3335abdedefae78201bb9071
sudexp <sudexp@icloud.com>
2018-11-28 11:46:23 +0200

    ShoppingCart.js: calculation methods created/ modified, style modified

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,6 +3,19 @@ import Navigation from './Navigation';
 import './ShoppingCart.css';
 
 class ShoppingCart extends Component {
+  totalPriceItem(id) {
+    return id.count * Number(id.item.price.substring(1));
+  }
+
+  totalPriceCart(cart) {
+    return Object.keys(cart).reduce((acc, id) => acc + this.totalPriceItem(cart[id]), 0);
+  }
+
+  toDelivery() {
+    const delivery = 10;
+    return delivery;
+  }
+
   render() {
     if (Object.keys(this.props.cart).length !== 0) {
       console.log('ShoppingCart: this.props.cart:', this.props.cart);
@@ -35,21 +48,24 @@ class ShoppingCart extends Component {
               ))}
             </div>
           </div>
-          <div className="em2.5 margin-left margin-right margin-top">Total: </div>
+          <div className="em2.5 margin-left margin-right margin-top animated fadeIn delay-1s">
+            Total: €{this.totalPriceCart(this.props.cart) + this.toDelivery()}
+          </div>
           <hr />
-          <div className="margin-top margin-left">
-            Items:
-            {Object.keys(this.props.cart).map(index => (
+          <div className="margin-top margin-left animated fadeIn delay-1s">
+            Items: €{this.totalPriceCart(this.props.cart)}
+            {/* {Object.keys(this.props.cart).map(index => (
               <div key={index}>
                 {this.props.cart[index].count * Number(this.props.cart[index].item.price.substring(1))}
+                {this.totalPriceItem(this.props.cart[index])}
               </div>
-            ))}
+            ))} */}
           </div>
-          <div className="margin-top margin-left">
+          <div className="margin-top margin-left animated fadeIn delay-1s">
             Delivery:
-            <div>10€</div>
+            <span> €10</span>
           </div>
-          <div className="margin-top col-12 text-center">
+          <div className="margin-top col-12 text-center animated fadeIn delay-2s">
             <button type="button" className="btn btn-primary">
               Continue to Pay
             </button>
