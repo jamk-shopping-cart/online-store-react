e40dfc12b451e553f5767ac579b9a708e04cc65e
sudexp <sudexp@icloud.com>
2018-12-04 17:45:56 +0200

    StartPage.js, SignIn.js, App.scss, variables.scss modified

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,18 +4,18 @@ import { Link } from './Router';
 class SignIn extends Component {
   render() {
     return (
-      <div className="wrap">
-        <div className="top em3">
+      <div className="wrap container-fluid d-flex h-100">
+        <div className="title top-title align-self-center em3">
           <Link to="collection">ShoeFlex</Link>
         </div>
-        <div className="center width80">
+        <div className="title align-self-center width80">
           <form id="signForm">
             <input id="username" type="text" placeholder="USERNAME" autoFocus required />
             <input id="password" type="password" placeholder="PASSWORD" required />
           </form>
           <p className="em2 font400">Log In</p>
         </div>
-        <div className="bottom">
+        <div className="subtitle align-self-center">
           <Link to="registration">
             <p className="em15 text-muted">Create an account</p>
           </Link>
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,11 +4,11 @@ import { Link } from './Router';
 class StartPage extends Component {
   render() {
     return (
-      <div className="wrap">
-        <div className="center font100">
+      <div className="wrap container-fluid d-flex h-100">
+        <div className="title align-middle align-self-center font-light">
           <p className="em3 animated fadeIn delay-0.5s">ShoeFlex</p>
         </div>
-        <div className="bottom font400 animated fadeIn delay-1s">
+        <div className="subtitle align-self-center font-bold animated fadeIn delay-1s">
           <p className="em15">View Our Collection 2018</p>
           <Link to="collection">
             <i className="fas fa fa-chevron-down em2" />
