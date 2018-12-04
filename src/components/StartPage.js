b6bdea1e61537b0f2982e17f5dfdfd93d1e02d48
sudexp <sudexp@icloud.com>
2018-12-04 23:31:31 +0200

    style for App.js, StartPage.js modified

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,15 +4,15 @@ import { Link } from './Router';
 class StartPage extends Component {
   render() {
     return (
-      <div className="container-fluid d-flex h-100 wrap">
+      <div className="wrap container-fluid d-flex h-100">
         <div className="row w-100 ml-auto mr-auto">
           <div className="title col-xs-12 col-sm12 col-md-12 col-lg-12 align-self-end font-light">
-            <p className="em3 animated fadeIn delay-0.5s">ShoeFlex</p>
+            <p className="animated fadeIn delay-0.5s">ShoeFlex</p>
           </div>
           <div className="subtitle col-xs-12 col-sm12 col-md-12 col-lg-12 align-self-center text-center font-bold animated fadeIn delay-1s">
-            <p className="em15">View Our Collection 2018</p>
+            <p>View Our Collection 2018</p>
             <Link to="collection">
-              <i className="fas fa fa-chevron-down em2" />
+              <i className="fas fa fa-chevron-down" />
             </Link>
           </div>
         </div>
