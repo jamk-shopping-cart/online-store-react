149144624e78439a75f9e074d15d8ff9ae38cb2e
sudexp <sudexp@icloud.com>
2018-12-05 00:00:59 +0200

    style for App.scss, SignIn.js modified

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,20 +5,22 @@ class SignIn extends Component {
   render() {
     return (
       <div className="wrap container-fluid d-flex h-100">
-        <div className="title top-title align-self-center em3">
-          <Link to="collection">ShoeFlex</Link>
-        </div>
-        <div className="title align-self-center width80">
-          <form id="signForm">
-            <input id="username" type="text" placeholder="USERNAME" autoFocus required />
-            <input id="password" type="password" placeholder="PASSWORD" required />
-          </form>
-          <p className="em2 font400">Log In</p>
-        </div>
-        <div className="subtitle align-self-center">
-          <Link to="registration">
-            <p className="em15 text-muted">Create an account</p>
-          </Link>
+        <div className="row w-100 ml-auto mr-auto">
+          <div className="title col-xs-12 col-sm12 col-md-12 col-lg-12 top-title align-self-start text-center">
+            <Link to="collection">ShoeFlex</Link>
+          </div>
+          <div className="form col-xs-12 col-sm12 col-md-12 col-lg-12 align-self-center">
+            <form id="signForm">
+              <input id="username" type="text" placeholder="USERNAME" autoFocus required />
+              <input id="password" type="password" placeholder="PASSWORD" required />
+            </form>
+            <p className="login font-bold">Log In</p>
+          </div>
+          <div className="subtitle col-xs-12 col-sm12 col-md-12 col-lg-12 align-self-center">
+            <Link to="registration">
+              <p className="text-muted">Create an account</p>
+            </Link>
+          </div>
         </div>
       </div>
     );
