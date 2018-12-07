c127d88dd007cea799e15509bcb40c60c17bcb73
sudexp <sudexp@icloud.com>
2018-12-07 16:15:03 +0200

    App.scss, ItemInfo.js, variables.scss modified (style for ItemInfo)

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -19,11 +19,11 @@ class ItemInfo extends Component {
       return (
         <React.Fragment>
           <Navigation />
-          <div className="left10 top5">
+          <div className="container-full text-center top">
             <Link to="collection">
-              <i className="fas fa-2x fa-arrow-left grey" />
+              <i className="fas fa-2x fa-arrow-left" />
             </Link>
-            <div className="margin-top">Please select an Item from the Collection</div>
+            <div className="mt-2">Please select an Item from the Collection</div>
           </div>
         </React.Fragment>
       );
@@ -31,30 +31,36 @@ class ItemInfo extends Component {
       return (
         <React.Fragment>
           <Navigation count={this.props.count} />
-          <div className="left10 top5" />
-          <div id="collection">
-            <div className="em2">{this.props.item.model}</div>
-            <div className="animated fadeIn delay-0.5s">
-              <img src={this.props.item.imgUrl} alt="shoes model" width={300} />
+          <div className="container-full top">
+            <div id="collection">
+              <div className="model text-center">{this.props.item.model}</div>
+              <div className="animated fadeIn delay-0.5s text-center">
+                <img src={this.props.item.imgUrl} alt="shoes model" width={300} />
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <span className="inline em25 margin-left-price">{this.props.item.price}</span>
-            <DropList setSize={this.setSize.bind(this)} />
-            <i className="fas fa-2x fa-cart-plus" onClick={this.handleClick.bind(this)} />
-          </div>
-          <div className="margin-top margin-left">
-            <b>Product information:</b>
-          </div>
-          <div className="margin-left margin-right justify">{this.props.item.description}</div>
-          <div className="margin-top margin-left">
-            <b>Color:</b> <span className="em12">{this.props.item.color}</span>
-          </div>
-          <div className="margin-left">
-            <b>Material:</b> <span className="em12">{this.props.item.material}</span>
-          </div>
-          <div className="margin-left">
-            <b>Closure method:</b> <span className="em12">{this.props.item.closure_method}</span>
+            <div className="row ml-2">
+              <span className="model">{this.props.item.price}</span>
+              <DropList setSize={this.setSize.bind(this)} />
+              <i className="fas fa-2x fa-cart-plus" onClick={this.handleClick.bind(this)} />
+            </div>
+            <div className="mt-3 mx-5">
+              <div>
+                <p className="feature text-left mb-0">Product information:</p>
+              </div>
+              <div className="text-justify">{this.props.item.description}</div>
+              <div>
+                <span className="feature">Color: </span>
+                <span className="description">{this.props.item.color}</span>
+              </div>
+              <div>
+                <span className="feature">Material: </span>
+                <span className="description">{this.props.item.material}</span>
+              </div>
+              <div>
+                <span className="feature">Closure method: </span>
+                <span className="description">{this.props.item.closure_method}</span>
+              </div>
+            </div>
           </div>
         </React.Fragment>
       );
